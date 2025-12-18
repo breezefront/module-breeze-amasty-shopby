4098ab879c3985a5324a1764b781248b5d6b0e63
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-12-18 11:58:24 +0200

    Fixes incorrect slider color and margins

--- view/frontend/web/js/components/amShopbyFilterSlider.js
+++ view/frontend/web/js/components/amShopbyFilterSlider.js
@@ -25,14 +25,22 @@ define([
                     self._setValue(newFrom, newTo, true);
                 }, 500),
                 mainColor = this.options.colors.main || '#000000',
-                hexToRgb = hex =>
-                    hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
-                        .substring(1).match(/.{2}/g)
-                        .map(x => parseInt(x, 16));
+                toRangeSliderColor = hexOrRgb => {
+                    if (hexOrRgb.startsWith('#')) {
+                        hexOrRgb
+                            .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
+                            .substring(1).match(/.{2}/g)
+                            .map(x => parseInt(x, 16));
+                    } else if (hexOrRgb.startsWith('rgb')) {
+                        return hexOrRgb.match(/\d+/g).map(Number);
+                    } else {
+                        return [0, 0, 0];
+                    }
+                };
 
             this.slider.hide();
 
-            range.attr('style', '--thumb-bg:' + hexToRgb(mainColor).join(' '));
+            range.attr('style', '--thumb-bg:' + toRangeSliderColor(mainColor).join(' '));
 
             range
                 .attr('min', minPrice)
