1145aedf739fa658322d2dfbd53c9b2faed30d8b
avanturist <tmvitaly@gmail.com>
2026-04-21 10:37:28 +0300

    Scroll into view for filter options 3 and 4.

--- view/frontend/web/js/amShopbyAjax.js
+++ view/frontend/web/js/amShopbyAjax.js
@@ -5,8 +5,15 @@ define([
  
     $.mixinSuper('amShopbyAjax', {
         scrollUp: function () {
+            var list = $(this.selectors.products_wrapper)[0];
+            
             if (this.options.scrollUp === 3 || this.options.scrollUp === 4) {
-                $(document).scrollTop(0);     
+                setTimeout(() => { // setTimeout uses for better behavior on mobile screen when navigation the filter options.
+                    list.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });                              
+                }, 200);
             }
         }
     });
